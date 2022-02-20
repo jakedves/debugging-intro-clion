fb2b2854d19a2568214a410a4bf9791e4433afac
Jake <jakedves@gmail.com>
2022-02-20 17:52:34 +0000

    Basic C Program

--- main.c
+++ main.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 
-struct Matrix2 {
-    double rows[2][2];
-};
-
 /**
- * Returns the determinant of a 2x2 square matrix
- * @param matrix : The matrix to compute with
- * @return : The determinant
+ * Returns the determinant of a 2x2 square matrix:
+ * [ a b ]
+ * [ c d ]
  */
-double determinant(struct Matrix2 matrix) {
-    double first = matrix.rows[0][0] * matrix.rows[1][1];
-    double second = matrix.rows[0][1] * matrix.rows[1][0];
-    return first - second;
+double determinant(double a, double b, double c, double d) {
+    return a * d - b * c;
 }
 
 int main() {
-    struct Matrix2 broken;
-    double top[] = {5, 0.1};
-    double bottom[] = {3, 5};
+    /**
+     * Create matrix:
+     * [5  0.1]
+     * [3  5]
+     */
+    double a = 5;
+    double b = 0.1;
+    double c = 3;
+    double d = 5;
 
-    printf("Hello, World!\n");
+
+    printf("Matrix:\n%f %f\n%f %f", a, b, c, d);
+    printf("\n\nDeterminant: %f", determinant(a, b, c, d));
+
     return 0;
 }
 
